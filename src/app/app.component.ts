5ff4fac9e96aee22abae719f8059d94e4d514a91
Adrian David <avdavid57@gmail.com>
2021-07-26 10:26:12 -0700

    Completed all sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string = '';
+  date: string = '';
+  amount: number = NaN;
+  miles: number = NaN;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  };
 
   onNameChange(event: Event) {
     const target = event.target as HTMLInputElement;
     this.name = target.value;
+  }
+
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.date = target.value;
+  }
+
+  onAmountChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.amount = parseFloat(target.value);
+  }
+
+  onMilesChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.miles = parseFloat(target.value);
   }
 }
